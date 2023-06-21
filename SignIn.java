d27c780f1aa1fd01b1458b000832730f08dd56e8
Harish-Kushwah <harishkushwah54321@gmail.com>
2023-06-21 19:17:41 +0530

    input feature added

--- SignIn.java
+++ SignIn.java
@@ -1,7 +1,13 @@
 import java.io.*;
 import java.util.*;
+import AccountPkg.*;
+import DesignPkg.Design;
 public class SignIn {
     public static void main(String[] args) {
-        System.out.println("Sign in page");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter name :");
+        String name = scan.nextLine();
+        System.out.println("Enter email");
+        String email =scan.nextLine(); 
     }
 }
